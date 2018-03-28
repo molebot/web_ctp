8d49ca562eb41d1095d98f1b7ac41fd70fd59f77
molebot <molebot@outlook.com>
2018-03-28 14:54:26 +0800

    账户管理器
    上传AccountPasswordManager
    可用于实盘账户管理，并支持测试账户临时借道

---
+++ AccountPassword.py
@@ -0,0 +1,24 @@
+# encoding: UTF-8
+
+class AccountPasswordManager:
+    Server_Dict = {}
+    Server_Dict['future_shanghai'] = {'md':'tcp://123.123.123.123:1234','td':'tcp://234.234.234.234:2345','brokerid':'1234'}
+    Account_Dict = {}
+    Account_Dict['user1234'] = {'name':u'测试用户','account':'12345678','password':'future','server':'future_shanghai'}
+    def get_account(self,account_id,other_info_dict = {}):
+        if account_id in self.Account_Dict:
+            _d = self.Account_Dict[account_id]
+            _server = _d.get('server','none')
+            if _server in self.Server_Dict:
+                _d['td'] = self.Server_Dict[_server]['td']
+                _d['md'] = self.Server_Dict[_server]['md']
+                _d['brokerid'] = self.Server_Dict[_server]['brokerid']
+            _d.update(other_info_dict)
+            return _d
+        else:
+            print(u'账户管理器未发现账户 %s'%account_id)
+            return {}
+
+AccountPasswordManager_Version = '2018-03-31'
+
+print(u'账户管理器加载完成 版本:%s'%AccountPasswordManager_Version)
